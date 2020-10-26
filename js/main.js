da99cd13286ee596263fcb6ceeb6c723dbfe1b87
Emma Dawson <emma.l.dawson@gmail.com>
2020-10-26 17:31:45 +0100

    added event listeners and comparison function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
 const btnLower = document.querySelector(".btn-lower")
 const btnSame = document.querySelector(".btn-same")
 const btnHigher = document.querySelector(".btn-higher")
+let deck = createCardDeck();
+let pointsCount = 0;
+let triesCount = 3;
+let cardsLeft = 52;
 
-function pickRandomCard() {
-  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+function createCardDeck() {
+  const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
   const suit = ["hearts", "diamonds", "clubs", "spades"]
-  let rand1 = (Math.floor(Math.random() * 12));
-  let rand2 = (Math.floor(Math.random() * 4));
-  let randomCard = [numbers[rand1], suit[rand2]]
-  console.log(randomCard);
+  const cardDeck = [];
+  for (let num of numbers) {
+    for (let s of suit) {
+      cardDeck.push([num, s])
+    }
+  } return cardDeck;
+}
+
+
+
+function pickRandomCard(deck) {
+  let randomNumber = (Math.floor(Math.random() * deck.length));
+  let randomCard = deck[randomNumber]
+  deck.splice(randomNumber, 1)
+  
   return randomCard
 }
+
+
 
 function showCard(card) {
   const cardNums = document.querySelectorAll("h6");
@@ -29,6 +46,8 @@ function showCard(card) {
     }
   })
 }
+
+
 
 function updatePoints() {
   const points = document.querySelector(".score");
@@ -49,19 +68,90 @@ function updateHowManyCards() {
 
 }
 
-  let prevCard = pickRandomCard();
-  let savePrevCardValue = prevCard[0];
-  let usedCards = [];
-  usedCards.push(prevCard.join(" "));
-  console.log(usedCards);
-  showCard(prevCard)
-  let pointsCount = 0;
-  let triesCount = 3;
-  let cardsLeft = 52;
+function initializeGame(deck) {
+  let randomCard = pickRandomCard(deck);
+  let prevValue = randomCard[0];
+  showCard(randomCard);
+  console.log(randomCard);
+  return prevValue;
+}
+
+let prevValue = initializeGame(deck)
+
+function compareCards(prev, current) {
+  if (prev === "J") {
+    prev = 10;
+  } else if (prev === "Q") {
+    prev = 11;
+  } else if (prev === "K") {
+    prev = 12;
+  } else if (prev === "A") {
+    prev = 13;
+  }
+  if (current === "J") {
+    current = 10;
+  } else if (current === "Q") {
+    current = 11;
+  } else if (current === "K") {
+    current = 12;
+  } else if (current === "A") {
+    current = 13;
+  }
+  if (prev > current) {
+    console.log(prev, current);
+    return "lower"
+  } else if (prev < current) {
+    console.log(prev, current);
+    return "higher"
+  } else {
+    console.log(prev, current);
+    return "equal"
+  }
+}
+
+
+function showCardOnButtonClick() {
+  updateHowManyCards()
+  let randomCard = pickRandomCard(deck);
+  console.log(randomCard);
+  showCard(randomCard);
+  let currentValue = randomCard[0]
+  return currentValue;
+  }
+    
 
 
 
-
-btnLower.addEventListener("click", function() {})
-btnSame.addEventListener("click", function() {})
-btnHigher.addEventListener("click", function() {})
+btnLower.addEventListener("click", function() {
+  let currentVal = showCardOnButtonClick()
+  let comparison = compareCards(prevValue, currentVal);
+  prevValue = currentVal;
+  if (comparison === "lower") {
+    updatePoints()
+  } else {
+    updateTries();
+  }
+   
+})
+btnSame.addEventListener("click", function() {
+  let currentVal = showCardOnButtonClick()
+  let comparison = compareCards(prevValue, currentVal);
+  prevValue = currentVal;
+  if (comparison === "equal") {
+    updatePoints()
+  } else {
+    updateTries();
+  }
+  
+})
+btnHigher.addEventListener("click", function() {
+  let currentVal = showCardOnButtonClick()
+  let comparison = compareCards(prevValue, currentVal);
+  prevValue = currentVal;
+  if (comparison === "higher") {
+    updatePoints()
+  } else {
+    updateTries();
+  }
+  
+})
